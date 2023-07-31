03a65f20497d4df66fc5019b7c33079447cb51de
peroxy <peroxy@users.noreply.github.com>
2023-07-31 17:14:44 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ const CLUSTER_URL = 'https://api.mainnet-beta.solana.com';
 async function main() {
     console.log('hello');
     const kamino = new Kamino('mainnet-beta', new Connection(CLUSTER_URL, 'confirmed'));
-    // const strategies = await kamino.getAllStrategiesWithFilters({});
-    // const msolStrategies = strategies.filter(
-    //     (x) =>
-    //         x.strategy.tokenAMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So' ||
-    //         x.strategy.tokenBMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'
-    // );
-    // for (const msolStrategy of msolStrategies) {
-    //     const holders = await kamino.getStrategyHolders(msolStrategy);
-    //     console.log(
-    //         'MSOL strategy:',
-    //         msolStrategy.address.toString(),
-    //         'amount of holders:',
-    //         holders.length
-    //     );
-    // }
+    const strategies = await kamino.getAllStrategiesWithFilters({});
+    const msolStrategies = strategies.filter(
+        (x) =>
+            x.strategy.tokenAMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So' ||
+            x.strategy.tokenBMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'
+    );
+    for (const msolStrategy of msolStrategies) {
+        const holders = await kamino.getStrategyHolders(msolStrategy);
+        console.log(
+            'MSOL strategy:',
+            msolStrategy.address.toString(),
+            'amount of holders:',
+            holders.length
+        );
+    }
 }
 
 (async () => {
