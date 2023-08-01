d31454cff30062ba68999dd155a4f076fa577f7b
peroxy <peroxy@users.noreply.github.com>
2023-08-01 12:45:20 +0200

    Update with example usage

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
 import { Kamino } from '@hubbleprotocol/kamino-sdk';
 import { Connection } from '@solana/web3.js';
+import Decimal from 'decimal.js';
 
-const CLUSTER_URL = 'https://api.mainnet-beta.solana.com';
+const CLUSTER_URL = 'enter RPC URL here';
 
 async function main() {
-    console.log('hello');
     const kamino = new Kamino('mainnet-beta', new Connection(CLUSTER_URL, 'confirmed'));
-    const strategies = await kamino.getAllStrategiesWithFilters({});
+    const strategies = await kamino.getStrategiesShareData({});
     const msolStrategies = strategies.filter(
         (x) =>
-            x.strategy.tokenAMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So' ||
-            x.strategy.tokenBMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'
+            (x.strategy.tokenAMint.toString() === 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So' ||
+                x.strategy.tokenBMint.toString() ===
+                    'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So') &&
+            new Decimal(x.strategy.sharesIssued.toString()).greaterThan(0)
     );
+    console.log(msolStrategies.length);
     for (const msolStrategy of msolStrategies) {
-        const holders = await kamino.getStrategyHolders(msolStrategy);
-        console.log(
-            'MSOL strategy:',
-            msolStrategy.address.toString(),
-            'amount of holders:',
-            holders.length
+        const holders = (await kamino.getStrategyHolders(msolStrategy)).filter((x) =>
+            x.amount.greaterThan(0)
         );
+        if (holders.length > 0) {
+            console.log(
+                '\nMSOL strategy:',
+                msolStrategy.address.toString(),
+                'amount of holders:',
+                holders.length
+            );
+            console.log('example stats of one holder: ', holders[0].holderPubkey.toString());
+            console.log('qty of ktokens: ', holders[0].amount.toString());
+            console.log(
+                'position usd value: ',
+                holders[0].amount.mul(msolStrategy.shareData.price).toString()
+            );
+        }
     }
 }
 
